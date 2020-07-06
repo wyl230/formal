defeed55f72fb18e244cfef30ab1583447a0e5b9
wyl230 <1900012779@pku.edu.cn>
2020-07-06 17:27:20 +0800

    r

--- un.py
+++ un.py
@@ -1,90 +1,100 @@
-import pgzrun  
-from math import * 
-from random import * 
+import pgzrun
+from math import *
+from random import *
 from pgzero.actor import Actor
 from pgzero.loaders import sounds
 from pgzero.clock import clock
 from pgzero.screen import Screen
-from pgzero.rect import Rect 
-screen: Screen  # 类型标注 
+from pgzero.rect import Rect
+screen: Screen  # 类型标注
 TITLE = 'undetermined'
+
+
 class Gameclass:
     def __init__(self):
         self.time = 0.
-        self.score = 0 
-        self.game_speed = 30 
-        self.time_elapsed = 0. 
-        self.blink = True  
-        self.n_frames = 0 
-        self.game_on = False 
-        self.game_message = 'fine' 
-        self.reset() 
+        self.score = 0
+        self.game_speed = 30
+        self.time_elapsed = 0.
+        self.blink = True
+        self.n_frames = 0
+        self.game_on = False
+        self.game_message = 'fine'
+        self.reset()
+
     def reset(self):
-        pass 
+        pass
+
     def check_game_over(self):
-        pass 
-WIDTH = 1000 
-HEIGHT = 1000 * 9 // 16 
+        pass
+
+
+WIDTH = 1000
+HEIGHT = 1000 * 9 // 16
 # star
 
-asters = [Actor('a'),Actor('b') ]
+asters = [Actor('a'), Actor('b')]
 
-ACCEL = 1.0 
-DRAG = 0.9 
-TRAIL_LENGTH = 2 
-MIN_WRAP_FACTOR = 0.1 
-BOUNDS = Rect(0,0,WIDTH,HEIGHT) 
+ACCEL = 1.0
+DRAG = 0.9
+TRAIL_LENGTH = 2
+MIN_WRAP_FACTOR = 0.1
+BOUNDS = Rect(0, 0, WIDTH, HEIGHT)
 FONT = 'eunomia_regular'
 
-warp_factor = MIN_WRAP_FACTOR 
-centerx = WIDTH // 2 
-centery = HEIGHT // 2 
-stars = [] 
+warp_factor = MIN_WRAP_FACTOR
+centerx = WIDTH // 2
+centery = HEIGHT // 2
+stars = []
+
 
 class Star:
     __slots__ = (
-        'pos','vel','brightness','speed','position_history' 
+        'pos', 'vel', 'brightness', 'speed', 'position_history'
     )
 
-    def __init__(self,pos,vel):
-        self.pos = pos 
-        self.vel = vel 
-        self.brightness = 10 
-        self.speed = hypot(*vel) 
+    def __init__(self, pos, vel):
+        self.pos = pos
+        self.vel = vel
+        self.brightness = 10
+        self.speed = hypot(*vel)
 
     @property
     def end_pos(self):
-        x,y = self.pos 
-        vx,vy = self.vel 
+        x, y = self.pos
+        vx, vy = self.vel
 
         return (
             x - vx * warp_factor * TRAIL_LENGTH / 60,
             y - vy * warp_factor * TRAIL_LENGTH / 60,
         )
 
-lastc = (1,1,1) 
+
+lastc = (1, 1, 1)
+
+
 def draw_stars():
-    f = lambda :randint(0,255) 
-    global lastc 
-    
-    if randint(1,120) != 1:
-        color = lastc 
+    def f(): return randint(0, 255)
+    global lastc
+
+    if randint(1, 120) != 1:
+        color = lastc
     else:
-        color =  (f(),f(),f()) 
-    lastc = color 
+        color = (f(), f(), f())
+    lastc = color
     for star in stars:
-        b = star.brightness 
-        cur_color = (int(i*b/5) for i in color) 
-        # color = (b*2,b*2,b*2) 
-        # color = (1,111,11) 
-        screen.draw.line(star.end_pos,star.pos,color) 
+        b = star.brightness
+        cur_color = (int(i*b/5) for i in color)
+        # color = (b*2,b*2,b*2)
+        # color = (1,111,11)
+        screen.draw.line(star.end_pos, star.pos, color)
+
 
 def update_stars(dt):
-    global stars,warp_factor 
+    global stars, warp_factor
     warp_factor = (
-        MIN_WRAP_FACTOR + (warp_factor - MIN_WRAP_FACTOR) * DRAG ** dt 
+        MIN_WRAP_FACTOR + (warp_factor - MIN_WRAP_FACTOR) * DRAG ** dt
     )
-
 
     while len(stars) < 300:
         # Pick a direction and speed
@@ -123,23 +133,46 @@ def update_stars(dt):
         if BOUNDS.collidepoint(star.end_pos)
     ]
 
+
+def line_towards(la, lb, scale):
+    d = lb - la + 1000
+    return la+d/scale, lb-d/scale
+
+
+def move_towards():
+    a = asters[0]
+    b = asters[1]
+    scale = 300
+    asters[0].x, asters[1].x = line_towards(a.x, b.x, scale)
+    a.y, b.y = line_towards(a.y, b.y, scale)
 # star
+
+
+def update_asters():
+    move_towards()
+
+
 def update(dt):
-    update_stars(dt) 
-    # if asters[0].x > asters[1].x*2:
-    asters[0].x += 30 
+    update_stars(dt)
+    update_asters()
+
 
 def draw():
-    screen.clear() 
+    screen.clear()
     for actor in asters:
         actor.draw()
-    draw_stars() 
-    # screen.fill('red') 
-    # screen.blit('background',(0,0)) 
+    draw_stars()
+    # screen.fill('red')
+    # screen.blit('background',(0,0))
+
 
 def on_mouse_down(pos):
-    global centerx,centery 
-    centerx,centery = pos 
+    global centerx, centery
+    centerx, centery = pos
+
+
 def on_key_down():
     pass
+
+
 pgzrun.go()
